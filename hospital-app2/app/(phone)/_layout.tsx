af7e94f3ffbbf858a08964cf94dffe3ffb2a5779
sofiav19 <sofiavitorica@gmail.com>
2026-03-24 18:01:46 +0100

    feat(layout): add TrackingInit component for enhanced tracking functionality

--- hospital-app2/app/(phone)/_layout.tsx
+++ hospital-app2/app/(phone)/_layout.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Slot } from "expo-router";
 import PhoneFrame from "../../components/shell/PhoneFrame";
 import NavDataInit from "../../components/data/NavDataInit";
+import TrackingInit from "../../components/data/TrackingInit";
 import AppScaffold from "../../components/shell/AppScaffold";
 
 export default function PhoneLayout() {
   return (
     <PhoneFrame>
       <NavDataInit />
+      <TrackingInit />
       <AppScaffold>
         <Slot />
       </AppScaffold>
