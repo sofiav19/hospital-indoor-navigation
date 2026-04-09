a6e6bec22c4cff383e2e16ac2466211ef9efc828
sofiav19 <sofiavitorica@gmail.com>
2026-04-09 21:31:26 +0200

    refactor(layout): replace PhoneFrame with AppScaffold and remove unused components

--- hospital-app2/app/(phone)/_layout.tsx
+++ hospital-app2/app/(phone)/_layout.tsx
@@ -1,19 +1,16 @@
 // app/(phone)/_layout.tsx
 import React from "react";
 import { Slot } from "expo-router";
-import PhoneFrame from "../../components/shell/PhoneFrame";
 import NavDataInit from "../../components/data/NavDataInit";
 import TrackingInit from "../../components/data/TrackingInit";
 import AppScaffold from "../../components/shell/AppScaffold";
 
 export default function PhoneLayout() {
   return (
-    <PhoneFrame>
+    <AppScaffold>
       <NavDataInit />
       <TrackingInit />
-      <AppScaffold>
-        <Slot />
-      </AppScaffold>
-    </PhoneFrame>
+      <Slot />
+    </AppScaffold>
   );
 }
